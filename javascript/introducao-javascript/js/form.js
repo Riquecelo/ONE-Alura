f46ae0da7240ccd0bd91e5fe0d298dabe1911638
Marcelo Santos <riquecelox462m@gmail.com>
2022-02-18 17:10:02 -0300

    validação de formulário com  msg de erro

--- javascript/introducao-javascript/js/form.js
+++ javascript/introducao-javascript/js/form.js
@@ -15,8 +15,7 @@ botaoPacente.addEventListener('click', function(e){
     var erros = validaPaciente(paciente);
 
     if(erros.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -25,7 +24,25 @@ botaoPacente.addEventListener('click', function(e){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML="";
 })
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML="";
+   /*maneira 1 de fazer
+    for(var i = 0; i < erros.length; i++){
+        var erro = erro[i];
+    }*/
+    //maneira 2 de fazer
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
     var paciente={
@@ -63,6 +80,19 @@ function montaTd(dado, classe){
 function validaPaciente(paciente){
 
     var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("Precisa adicionar um nome!")
+    }
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco")
+    }
+    if(paciente.altura.length == 0){
+        erros.push("Altura não pode ser em branco")
+    }
+    if(paciente.peso.length == 0){
+         erros.push("A altura não pode ser em branco")
+    }
 
     if(!validaPeso(paciente.peso)){
         erros.push("Peso inválido");
