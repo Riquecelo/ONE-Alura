647a444ace9bc86279d73aa7caafc0821fdcb197
Marcelo Santos <riquecelox462m@gmail.com>
2022-02-22 12:43:57 -0300

    tratando erros de requisição

--- javascript/introducao-javascript/js/buscar-pacientes.js
+++ javascript/introducao-javascript/js/buscar-pacientes.js
@@ -2,19 +2,31 @@ var botaoAdicionar = document.querySelector("#buscar-paciente")
 
 botaoAdicionar.addEventListener('click', function(){
     //console.log(botaoAdicionar)
-
     //requisição com javascript
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacienteJson = JSON.parse(resposta);
 
-        pacienteJson.forEach(function(paciente) {
-            adicionaPacienteNatabela(paciente)
-        });
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel")
+            var resposta = xhr.responseText;
+            var pacienteJson = JSON.parse(resposta);
+
+            pacienteJson.forEach(function(paciente) {
+                adicionaPacienteNatabela(paciente)
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            
+            erroAjax.classList.remove("invisivel");
+            //console.log(erroAjax)
+           
+        } 
     })
 
     xhr.send()
