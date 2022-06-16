0982e28c9c51282ffef81ef9803568623dc37aac
Marcelo_Santos <riquecelox462m@gmail.com>
2022-06-15 23:53:40 -0300

    atualização

---
+++ javascript/oo/ContaCorrente.js
@@ -0,0 +1,28 @@
+
+export class ContCorrente{
+    
+    agencia;
+    cliente;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        } 
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;  
+        }
+        this._saldo += valor;
+    }
+
+    transferir (valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.cidade='São Paulo';
+        conta.depositar(valorSacado);
+    
+    }
+}
--- javascript/oo/index.js
+++ javascript/oo/index.js
@@ -1,26 +1,5 @@
-class Cliente {
-    nome;
-    cpf;   
-    rg;
-}
-
-class ContCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-        }
-    }
-
-    depositar(valor){
-        if(this._saldo >= 0){
-            this._saldo += valor;
-        }
-    }
-}
-
+import { Cliente } from "./Cliente.js";
+import { ContCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Marcelo";
@@ -31,9 +10,30 @@ const cliente2 = new Cliente();
 cliente2.nome = "Henrique";
 cliente2.cpf = 11122233367;
 
+
 const contaCorrenteHenrique = new ContCorrente();
 
+contaCorrenteHenrique.cliente =cliente2;
 contaCorrenteHenrique.agencia = 1001;
-contaCorrenteHenrique.depositar(100);
-contaCorrenteHenrique.depositar(50)
-console.log(contaCorrenteHenrique.saldo)
+contaCorrenteHenrique.depositar(500);
+//contaCorrenteHenrique.depositar(50)
+//console.log(contaCorrenteHenrique.saldo)
+
+//const valorSacado = contaCorrenteHenrique.sacar(50)
+
+//console.log(valorSacado)
+//console.log(contaCorrenteHenrique)
+
+const cliente3 = new Cliente();
+cliente3.nome = "Alice";
+cliente3.cpf = 789456789;
+
+const conta3 = new ContCorrente();
+conta3.cliente = cliente3;
+conta3.agencia = 102;
+
+//console.log(conta3)
+
+contaCorrenteHenrique.transferir(200,conta3)
+console.log(conta3)
+console.log(contaCorrenteHenrique)
