e6f85617e4a115ef127b3fa40259ac8c3b7b7ee2
Marcelo_Santos <riquecelox462m@gmail.com>
2022-06-27 00:23:23 -0300

    Trilha FrontEnd Curso de DOM - manipulação de elementos

--- DOM-projeto_incial/listaDeTarefas.js
+++ DOM-projeto_incial/listaDeTarefas.js
@@ -1,5 +1,42 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+//Utilizando a “IIFE”, ou Immediately Invoked Function Expression ou “Função de Invocação Imediata”.
 
-novaTarefa.addEventListener('click', ()=>{
-    console.log('fui clicado!')
-})
+(() => {
+    const criarTarefa = (evento) => {
+        evento.preventDefault()
+
+        const lista = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
+
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
+        const conteudo = `<p class="content">${valor}</p>`
+
+        tarefa.innerHTML = conteudo
+
+        tarefa.appendChild(BotaoConclui())
+        lista.appendChild(tarefa)
+        input.value = " "
+    }
+
+    const novaTarefa = document.querySelector('[data-form-button]')
+
+    novaTarefa.addEventListener('click', criarTarefa)
+
+    const BotaoConclui = () => {
+        const botaoConclui = document.createElement('button')
+
+        botaoConclui.classList.add('check-button')
+        botaoConclui.innerText = 'concluir'
+        botaoConclui.addEventListener('click', concluirTarefa)
+        return botaoConclui
+    }
+
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target
+
+        const tarefaCompleta = botaoConclui.parentElement
+        tarefaCompleta.classList.toggle('done')
+    }
+
+})()
